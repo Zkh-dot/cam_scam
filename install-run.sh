265bfddb2ef12560f2bfd4801c4106539ad56915
Zkh-dot <kernzahar@gmail.com>
2024-07-19 23:29:31 +0300

    support ubuntu and debian installation

--- install-run.sh
+++ install-run.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+
+# go to code dir
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
+# default values
 install_flag=false
 button="f12"
 only="all"
 
+# detect system packet manager
+declare -A osInfo;
+osInfo[/etc/arch-release]="pacman -S"
+osInfo[/etc/debian_version]="apt-get install"
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        pack_manager=${osInfo[$f]}
+    fi
+done
+if [ -z ${pack_manager+x} ]; then 
+  echo "we have no idea, what this system is, sorry"
+  exit 255
+fi
+
+# get command line arguments
 while getopts 'irb:a:m:o:' OPTION; do
   case "$OPTION" in
     i)
@@ -39,7 +59,7 @@ done
 shift "$(($OPTIND -1))"
 
 
-if [ -d "./cam_scam_venv" ] || [ $install_flag = false ]; then
+if [ -d "./cam_scam_venv" ] && [ $install_flag = false ]; then
     printf "everything seems installed, running...\n"
     printf "running app...\n"
     if [ -z ${audio_file+x} ] && [ $only != "cam" ]; then
@@ -56,13 +76,21 @@ if [ -d "./cam_scam_venv" ] || [ $install_flag = false ]; then
     ./cam_scam_venv/bin/python3 button_registration.py $button $only $mic_id $audio_file # > ./logs/logs.log
 else
     printf "installing headers..."
-    sudo pamac install $(pamac list --quiet --installed | grep "^linux[0-9]*[-rt]*$" | awk '{print $1"-headers"}' ORS=' ')
-    pacman -S v4l2loopback-dkms v4l2loopback-utils  
+    if [ "$pack_manager" == "pacman -S" ]; then
+      sudo pamac install $(pamac list --quiet --installed | grep "^linux[0-9]*[-rt]*$" | awk '{print $1"-headers"}' ORS=' ')
+    else
+      sudo apt-get install linux-headers-$(uname -r);
+    fi
+    $pack_manager v4l2loopback-dkms v4l2loopback-utils  
     v4l2-ctl --list-devices
     printf "creating venv..."
+    if [ "$pack_manager" == "apt-get install" ]; then
+      sudo $pack_manager python3.$(python3 -c 'import sys;print(sys.version_info[:][1])')-venv
+      # sudo $pack_manager python3-pip -y
+    fi
     python3 -m venv ./cam_scam_venv
-    sudo pacman -S gcc
+    sudo $pack_manager gcc
     printf "installing requirements..."
-    ./cam_scam_venv/bin/python3 -m pip install -r ./requirements.txt
+    ./cam_scam_venv/bin/pip3 install -r ./requirements.txt
     mkdir logs
 fi
